ca0cd639dd102d3b2f66a00f537219b5a6c58de5
Marino Rosić <56385553+MarinoRosic@users.noreply.github.com>
2024-04-13 22:04:09 +0200

    Logo na Hero sectionu i favicon

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -1,16 +1,18 @@
 import React from 'react'
 // import testSvg from "../assets/images/test.svg"
+import pepLogo from "../assets/images/PEPLogo.png"
 
 const HeroSection = () => {
   return (
     <>
         <section className='flex flex-col mx-auto h-[100vh] w-full'>
-            {/* <div className=' w-full h-full absolute -z-20'>
+            {/* <div className='absolute w-full h-full -z-20'>
                 <img src={testSvg} className='mx-[400px] rotate-90 h-[950px]' alt="" />
             </div> */}
-            <header className='mx-auto my-auto flex flex-col'>
-                <p className='text-white text-9xl font-extrabold'>PEP</p>
-                <p className='text-white text-9xl font-semibold'>KONF</p>
+            <header className='flex mx-auto my-auto'>
+                {/* <p className='font-extrabold text-white text-9xl'>PEP</p>
+                <p className='font-semibold text-white text-9xl'>KONF</p> */}
+                <img className='' src={pepLogo}></img>
             </header>
         </section>
     </>
