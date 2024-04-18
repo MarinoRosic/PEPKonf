90438f35f7c292526bc7d28572e3a525c07c4aba
Marino Rosić <56385553+MarinoRosic@users.noreply.github.com>
2024-04-18 20:05:14 +0200

    Razmak između, početna sekcija

--- src/Components/HomeSection.js
+++ src/Components/HomeSection.js
@@ -7,7 +7,7 @@ const HomeSection = () => {
     const isInView = useInView(ref, {once: true});
   return (
     <>
-        <section className='bg-stolice-BG-Mob lg:bg-stolice-BG h-[100vh] overflow-hidden bg-cover flex flex-col items-center gap-y-6'>
+        <section className='bg-stolice-BG-Mob lg:bg-stolice-BG h-[100vh] overflow-hidden bg-cover flex flex-col items-center gap-y-10'>
             <motion.div 
             className='mx-auto h-[33%] content-center'
             initial={{ opacity: 0, scale: 0.6 }}
@@ -36,7 +36,7 @@ const HomeSection = () => {
             x: { duration: 1, type: "tween" }
             }} 
             >
-                <h1 className='sporka text-4xl font-bold md:text-7xl lg:text-8xl mx-auto text-center'>Iskustvo i znanje <br /> iz PRve ruke.</h1>    
+                <h1 className='sporka text-4xl font-extrabold md:text-7xl lg:text-8xl mx-auto text-center'>Iskustvo i znanje <br /> iz PRve ruke.</h1>    
             </motion.div>
             <motion.div 
             className='h-[33%] content-center'
@@ -48,7 +48,7 @@ const HomeSection = () => {
             x: { duration: 0.9, type: "tween" }
             }} 
             >
-                <p className='text-xl font-thin text-white italic text-center'>25. - 26. travnja 2024. <br /> Sveučilišni kampus u Dubrovniku</p>
+                <p className='text-lg font-thin text-white italic text-center'>25. - 26. travnja 2024. <br /> Sveučilišni kampus u Dubrovniku</p>
             </motion.div>
         </section>
     </>
