76e38d87606d934718ae935acceacabb1b889c4b
Marino Rosić <56385553+MarinoRosic@users.noreply.github.com>
2024-04-21 10:47:01 +0200

    Sitne izmjene

--- src/Components/Partners.js
+++ src/Components/Partners.js
@@ -19,7 +19,7 @@ const Partners = () => {
                 </div>
             </div>
             <div className='absolute bottom-0 h-[50px] border-t-[#261539] border-t-2 w-full bg-[#261539] px-2'>
-                <div className='flex flex-row flex-grow h-full '>
+                <div className='flex flex-row content-center flex-grow h-full'>
                     <p className='text-sm span font-thin w-[33%] justify-center text-left content-center'><a href="">Privacy Policy</a></p>
                     <p className='w-[33%] text-sm span text-center justify-center content-center'>Copyright © 2024 PEP Konf</p>
                     <p className='span font-thin text-sm w-[33%] justify-center text-right content-center'>Made by <a href="https://www.afdu.dev"><span className='text-[#F0E2FF]'>AFDU</span></a></p>
