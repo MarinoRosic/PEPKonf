b405ef3d9d50ff1acac3003d38954ecd3eb3128b
Marino Rosić <56385553+MarinoRosic@users.noreply.github.com>
2024-04-23 22:39:46 +0200

    Dorade na Programu i WWW sekciji na velikom ekranu.

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,30 +1,15 @@
 import {React} from 'react'
-// import rasporedSlika from "../assets/images/rasporedStrana.png"
 import { motion } from 'framer-motion'
 
 const Table = ({datum, day, partyTime, partyLocation}) => {
   return (
     <>
-        <section className='flex flex-col w-full lg:h-[100vh]'>
+        <section className='flex flex-col w-full'>
             <motion.h1 
             className='text-[#772F6F] text-3xl lg:text-6xl mx-auto pt-16 lg:pt-28 font-extrabold'
-            // initial={{ x: "-100%" }}
-            // animate={{ x: 0 }}
-            // transition={{
-            // ease: "linear",
-            // duration: 1,
-            // x: { duration: 1, type: "tween" }
-            // }} 
             >{datum}</motion.h1>
             <motion.div 
-            className='flex flex-col items-start px-12 pt-12 lg:mx-52 lg:pt-12'
-            // initial={{ x: "-100%" }}
-            // animate={{ x: 0 }}
-            // transition={{
-            // ease: "linear",
-            // duration: 1,
-            // x: { duration: 1, type: "tween" }
-            // }} 
+            className='flex flex-col justify-center px-12 pt-12 lg:mx-auto lg:pt-12'
             >
                 {day.map((items, index) => {
                     const {time, tema} = items
@@ -33,23 +18,10 @@ const Table = ({datum, day, partyTime, partyLocation}) => {
             </motion.div>
             <motion.div 
             className='z-10 pt-12 pb-5 border-b-[#db9bd5] border-b-4 mx-auto font-extrabold md:pt-32 lg:pt-36'
-            // initial={{ x: "-100%" }}
-            // animate={{ x: 0 }}
-            // transition={{
-            // ease: "linear",
-            // duration: 1,
-            // x: { duration: 1, type: "tween" }
-            // }} 
             >
                 <h1 className='font-bold text-[#db9bd5] text-xl lg:text-5xl'>{partyLocation}</h1>
                 <h1 className='text-center text-[#db9bd5] text-xl lg:text-5xl'>{partyTime}</h1>
             </motion.div>
-            {/* <div className='absolute pt-80 lg:pt-20'>
-                <img className='lg:h-full lg:w-full h-[350px] w-[350px]' src={rasporedSlika} alt="" />
-            </div>
-            <div className='absolute right-0 pt-80 lg:pt-20'>
-                <img className='lg:h-full lg:w-full h-[350px] w-[350px] img-hor' src={rasporedSlika} alt="" />
-            </div> */}
         </section>
     </>
   )
