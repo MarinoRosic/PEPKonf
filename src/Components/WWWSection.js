e76117f0a3dcb5ff08165557e51f821d217e191c
Marino Rosić <56385553+MarinoRosic@users.noreply.github.com>
2024-04-23 21:59:16 +0200

    WWW sekcija

--- src/Components/WWWSection.js
+++ src/Components/WWWSection.js
@@ -5,7 +5,7 @@ const WWWSection = () => {
   return (
     <>
         <section className='flex h-[100vh] font-main overflow-hidden'>
-            <div className='flex flex-col p-8 mx-auto md:px-20 content-center justify-center'>
+            <div className='flex flex-col p-8 mx-auto md:px-20 content-center justify-center items-center'>
                 <WWWCard question="ŠTO?" answer="PEP konferencija je događaj koji okuplja istaknute predavače iz područja odnosa s javnošću i novinarstva kako bi podijelili svoje znanje, iskustvo i najnovije trendove sa studentima i mladima." />
                 <WWWCard question="KAD?" answer="Četvrtak & petak | 25. i 26. travnja 2024." />
                 <WWWCard question="GDJE?" answer="Sveučilišni kampus, Ulica branitelja Dubrovnika 41, Dubrovnik" />
