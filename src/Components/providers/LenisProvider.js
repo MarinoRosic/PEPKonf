0b19619f375e8d6ad57a35137739e9aa74dc75f0
Marino Rosić <56385553+MarinoRosic@users.noreply.github.com>
2026-04-09 22:51:39 +0200

    Fix - Lenis horizontal scroll on text

--- src/Components/providers/LenisProvider.js
+++ src/Components/providers/LenisProvider.js
@@ -11,7 +11,7 @@ export default function LenisProvider({ children }) {
         easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
         smoothWheel: true,
         smoothTouch: false,
-        syncTouch: true,
+        syncTouch: false,
       }}
     >
       {children}
